be577b98a7278972028e4455db1921a1a3b50d7e
Ramya Gunashekara <ramya.gunashekar@gmail.com>
2021-05-10 07:24:38 +0530

    Added test cases

--- src/main/java/everest/engineering/data/OfferTableModel.java
+++ src/main/java/everest/engineering/data/OfferTableModel.java
@@ -15,7 +15,8 @@ public class OfferTableModel extends AbstractTableModel {
 			{ "OFR002", Integer.valueOf(7), Range.between(50.0, 150.0), Range.between(100.0, 250.0) },
 			{ "OFR003", Integer.valueOf(5), Range.between(50.0, 250.0), Range.between(10.0, 150.0) },
 			{ "OFR004", Integer.valueOf(20), Range.between(100.4, 250.0), null },
-			{ "OFR005", Integer.valueOf(6), null, null } };
+			{ "OFR005", Integer.valueOf(6), null, null },
+			{ "OFR006", Integer.valueOf(9), Range.between(0.0, 1000.0), Range.between(0.00, 1000.0) } };
 
 	private String[] columnNames = { "ID", "DISCOUNT", "DISTANCE", "WEIGHT" };
 
